46c63321b52c0a61607f429906be4eb0d97aac96
Daniil <ksusonic@yandex.ru>
2021-05-05 11:12:53 +0300

    Sending response and endless loop version

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Simple http server on Python 3.9
+
+
+### Launch:
+```python
+python server.py
+```
+
+### Recieves connection on 127.0.0.1:8000
+
+### Returns response on `http://127.0.0.1:8000/mypath`:
+```html
+<body>
+Привет!
+Path: /mypath
+</body>
+```
--- server.py
+++ server.py
@@ -5,8 +5,17 @@ server = socket.create_server(('127.0.0.1', 8000))
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server.listen(10)
+try:
+    while True:
+        client_socket, address = server.accept()
+        recieved_data = client_socket.recv(1024).decode('utf-8')
 
-client_socket, address = server.accept()
-recieved_data = client_socket.recv(1024).decode('utf-8')
+        print("Получены данные по сокету", recieved_data)
 
-print("Получены данные по сокету", recieved_data)
+        path = recieved_data.split(" ")[1]
+        response = f"HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8\n\n" \
+            f"Привет!<br /> Path: {path}"
+        client_socket.send(response.encode("utf-8"))
+except KeyboardInterrupt:
+    client_socket.shutdown(socket.SHUT_RDWR)
+    server.close()
